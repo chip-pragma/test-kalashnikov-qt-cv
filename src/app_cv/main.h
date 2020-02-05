6a57f6c44874aaa093a73aa254e60fb3872b7b7a
Ivan Cheltsov <chip.pragma@gmail.com>
2020-02-05 23:35:56 +0300

    Вывод без обработки (и семафоров)

---
+++ src/app_cv/main.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/videoio.hpp>
+
+#include "core/options.h"
+#include "core/defines.h"
+
+void processMats(cv::Mat &mat1, cv::Mat &mat2);
